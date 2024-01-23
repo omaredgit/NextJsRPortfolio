9e1693632f8d125850be5bafe30a494c4ac37ccb
omar eduardo carvajal guerra <carvajalomar123@gmail.com>
2024-01-23 16:54:00 -0500

    add blackhole Video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,14 @@
 import Image from 'next/image'
+import Hero from './components/main/Hero'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full items-center justify-center font-mono text-lg lg:flex">
-      <img className='h-24' src="https://cdn-icons-png.flaticon.com/512/6840/6840478.png" alt="alternative-text"/>        
-        <div className="flex w-full justify-center items-center">
-          <p> 
-        &lt;Hello World/&gt;
-        EagleBear PowerCore
-        </p>
-        </div>
-        <img className='h-24' src="https://cdn-icons-png.flaticon.com/512/6840/6840478.png" alt="alternative-text"/>        
+    <main className="h-full w-full">
+
+      <div className='flex flex-col h-[850px] gap-20'>
+      <Hero/>
+
       </div>
-      <div className="z-10 w-full items-center justify-center font-mono text-lg lg:flex">
-
-      <div className="flex justify-center items-center">
-        <p>
-        My Name is Omar Carvajal
-        </p>
-        </div>
-        </div>
-        <div className="z-10 w-full items-center justify-center font-mono text-lg lg:flex">
-
-      <div className="flex justify-center items-center ">
-        <p>
-        I will be sharing with you all my portfolio really soon
-        </p>
-        </div>
-        </div>
 
     </main>
   )
