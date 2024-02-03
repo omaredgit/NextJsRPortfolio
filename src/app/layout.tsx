74dd00845b728707cc29424c6b98b21fead148d1
omar eduardo carvajal guerra <carvajalomar123@gmail.com>
2024-02-03 01:16:16 -0500

    added publics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,24 +2,29 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import StarBackground from './components/main/StarBackground'
-
-const inter = Inter({ subsets: ['latin'] })
+import NavBar from './components/main/NavBar'
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Omar portfolio',
-  description: "This is the Omar's portfolio",
-}
+  title: "Space Portfolio",
+  description: "This is my portfolio",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden` }>
-        <StarBackground/>
-        {children}</body>
+      <body
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarBackground />
+        <NavBar />
+        {children}
+        {/* <Footer /> */}
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
 import Hero from './components/main/Hero'
+import Skills from './components/main/Skills'
+import Encryption from './Encryption'
 
 export default function Home() {
   return (
     <main className="h-full w-full">
 
-      <div className='flex flex-col h-[850px] gap-20'>
+      <div className='flex flex-col gap-20'>
       <Hero/>
-
+      <Skills/>
+      <Encryption />
       </div>
 
     </main>
